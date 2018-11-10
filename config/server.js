794b24e34b0089ed03b7650bc856b67b1a8ac399
Fabrĩcio Ribeiro <fabricio@fabriciolribeiro.com>
2018-11-10 10:07:12 -0200

    Projeto de chat para estudo de Node.js

---
+++ config/server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+
+var consign = require('consign');
+
+var bodyParser = require('body-parser');
+
+var expressValidator = require('express-validator');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+app.use(express.static('./app/public'));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(expressValidator());
+
+/* Efetua autoload das rodas, models e controllers para o objeto app. */
+consign()
+    .include('app/routes')
+    .then('app/models')
+    .then('app/controllers')
+    .into(app);
+
+module.exports = app;
